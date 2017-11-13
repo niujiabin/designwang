f085f2ad0e8fc55ce307c4d06eda6dfe25866f1a
niiujiabin <njb19920913@163.com>
2017-11-13 23:22:05 +0800

    学生管理

--- src/main/java/design/job/controller/JsonResult.java
+++ src/main/java/design/job/controller/JsonResult.java
@@ -3,7 +3,7 @@ package design.job.controller;
 import java.util.List;
 import java.util.Map;
 
-//KKKKKKKKKKKKK666777
+//KKKKKKKKKKKKK666777778878
 public class JsonResult {
 
     public String code;
